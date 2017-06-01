8087c25396e002dc712d44c492f24516ee37e091
Anvita Achar <anvita.achar@gmail.com>
2017-06-01 07:41:32 -0400

    removed comments and converted strings to their hex values

--- obfuscate.py
+++ obfuscate.py
@@ -10,6 +10,8 @@ seed = random.randint(0,500)
 
 variables = {}
 def replace_variables (code):
+
+	code = remove_comments(code)
 	
 	# variables contains the names of all the 
 	# variables that have been assigned values in the code
@@ -17,8 +19,7 @@ def replace_variables (code):
 	for var in re.finditer(r"([\$\w]+)\s*(=|\()", code):
 		global variables 
 		variables[var.group(1)] = random_name_generator()
-	print variables
-	newCode = code
+ 	newCode = " "+code
 	for var in variables:
 		newCode = re.sub( r'(\s+)'+r'('+re.escape(var)+r')'+r'(\s*|\.|\()', r'\1'+variables[var]+r'\3', newCode)
 	return newCode	
@@ -50,21 +51,24 @@ def remove_strings(js_file):
 	for string in re.finditer(r"\'[^\']+\'", code): 
 		# add string to array
 		stringArray[string.group(0)] = True
-	jsarray = "var _"+ hex(seed)+"={"
+	jsarray = "var _"+ hex(seed)+"=["
 	for idx,item in enumerate(stringArray):
-		jsarray = jsarray + item + ","
+		jsarray = jsarray +"\"" +convert_string_to_hex(item) + "\""+ ","
 		newcode = re.sub(re.escape(item),hex(seed)+"["+str(idx)+ "]", newcode)
 		newcode = re.sub(re.escape(item),hex(seed)+"["+str(idx)+ "]", newcode)
 	
-	jsarray = jsarray[:-1]+"};"
+	jsarray = jsarray[:-1]+"];"
 	return (jsarray, newcode)
+def convert_string_to_hex(string):
+	return re.sub(r'(.{2})', r'\x\1', string.encode("hex"))
 
 def store_strings(strings, code):
-
 	return strings + code	
 
+def remove_comments(string):
+	return re.sub(r'(\/\*(.|\n)*\*\/)|(\/\/.*\n)','',string)
+
 result = remove_strings(sys.argv[1])
-print result
 stringarray = result[0]
 code = result[1]
 print stringarray+remove_spaces(replace_variables(code))
--- test.js
+++ test.js
@@ -1,12 +1,15 @@
 $d = 3;
-
+$d.lol = 6;
 _sdfkj = 1;
-$d.lol;
+// $d.lol;
 var xar = 5;
 y = 6;
-z = xar + y;
+z = xar + y; 
+/*dfksjhskjhgsjkhgkj 
+skjhfkgjh slkfjslgjs*/
 function anvita(){
 	string = "hi plz xar";
 	ox = "sadlajs";
+	window.alert(z+ox);
 }
 anvita();
